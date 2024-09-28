bbaa8084b79f2e1f18f0fca8978de369ad34443e
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-09-28 20:04:51 +0000

    terminado

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,12 +5,13 @@ import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
+import { QuienSomos } from "./pages/quienSomos"
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-
+ 
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -27,6 +28,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<QuienSomos />} path="/quienSomos" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
