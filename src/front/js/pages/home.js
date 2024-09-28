ccabe40501a2be17ae2023fa89256289be11e840
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-09-28 20:16:29 +0000

    terminado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,23 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css'; 
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+const Jumbotron = () => {
+  return (
+    <div className="container mt-5">
+      <div className="jumbotron">
+        <div className="todo">
+          <h1 className="display-4">¡Bienvenido a Abi&a!</h1>
+          <p className="lead">Mejora cada mañana con buen café.</p>
+          <p>Explora nuestros productos y encuentra tu sabor perfecto.</p>
+          <p>Café de especialidad en grano o molido en sólo un click.</p>
+          <a className="btn btn-custom btn-lg" href="#" role="button" style={{ color: '#4B2E0F' }}>
+            Pide tu café
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Jumbotron;
