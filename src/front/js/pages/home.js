6f3c9a446a25f1da3cee4b4646f03dd1f95f5d38
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-09-29 19:45:15 +0000

    jumbotron casi todo arreglado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../styles/home.css'; 
+import jumbotron from "../../img/jumbotron.jpeg";
 
 const Home = () => {
   return (
     <div className="container mt-5">
-      <div className="jumbotron">
-        <div className="todo">
+      <div className="jumbotron position-relative">
+      <img className='position-absolute jumbotron-imagen' src={jumbotron} />
+        <div className="todo position-relative">
           <h1 className="display-4">¡Bienvenido a Abi&a!</h1>
           <p className="lead">Mejora cada mañana con buen café.</p>
           <p>Explora nuestros productos y encuentra tu sabor perfecto.</p>
